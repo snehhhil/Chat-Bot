9a8b8e51d9b676b50e82c98999614a10807aa8ff
Snehil <130927955+snehhhil@users.noreply.github.com>
2025-01-08 09:13:18 +0530

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Chatbot from './components/Chatbot';
+import './App.css'; // Import the new CSS file
+
+const App = () => {
+    return (
+        <div className="App">
+            <Chatbot />
+        </div>
+    );
+};
+
+export default App;
